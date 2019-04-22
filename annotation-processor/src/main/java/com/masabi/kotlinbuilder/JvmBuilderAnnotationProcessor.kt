6d16c277738d0f981bacb5666afcec1082a6876c
Colin Vipurs <colin.vipurs@masabi.com>
2019-04-22 09:38:02 +0100

    latest build

--- annotation-processor/src/main/java/com/masabi/kotlinbuilder/JvmBuilderAnnotationProcessor.kt
+++ annotation-processor/src/main/java/com/masabi/kotlinbuilder/JvmBuilderAnnotationProcessor.kt
@@ -8,12 +8,14 @@ import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
 import com.masabi.kotlinbuilder.JvmBuilderAnnotationProcessor.BuilderField
 import com.masabi.kotlinbuilder.annotations.JvmBuilder
 import com.squareup.kotlinpoet.*
+import me.eugeniomarletti.kotlin.metadata.*
+import me.eugeniomarletti.kotlin.metadata.shadow.metadata.ProtoBuf
+import me.eugeniomarletti.kotlin.metadata.shadow.metadata.deserialization.NameResolver
+import me.eugeniomarletti.kotlin.metadata.shadow.serialization.deserialization.getName
 import java.io.File
 import javax.annotation.processing.*
 import javax.lang.model.SourceVersion
-import javax.lang.model.element.Element
-import javax.lang.model.element.TypeElement
-import javax.lang.model.element.VariableElement
+import javax.lang.model.element.*
 import javax.lang.model.type.TypeMirror
 import javax.lang.model.util.ElementFilter
 import javax.tools.Diagnostic.Kind.ERROR
@@ -69,16 +71,50 @@ class JvmBuilderAnnotationProcessor : AbstractProcessor() {
     }
 
     private fun propertiesFrom(targetClass: Element): List<BuilderField> {
+        val typeMetadata = targetClass.kotlinMetadata as KotlinClassMetadata
+        val nameResolver = typeMetadata.data.nameResolver
+        val classProto = typeMetadata.data.classProto
+
         val constructors = ElementFilter.constructorsIn(targetClass.enclosedElements)
 
         if (constructors.size == 0) {
             processingEnv.messager.printMessage(ERROR, "No primary constructor found")
         }
 
+
+        /******/
+        val typeResolver = TypeResolver()
+        val propertyList = classProto.propertyList
+        val element = constructors.first()
+        val constructor = classProto.constructorList.single { it.isPrimary }
+        for (parameter in constructor.valueParameterList) {
+            val name = nameResolver.getString(parameter.name)
+            val index = constructor.valueParameterList.indexOf(parameter)
+            val hasDefault = parameter?.declaresDefaultValue ?: true
+            val hasDefault = parameter?. ?: true
+
+            log("things =====> \t\tname = $name, paramter = $parameter, hasDefault => $hasDefault")
+        }
+
+//        for (property in propertyList) {
+//            val name = nameResolver.getString(property.name)
+//            val type = typeResolver.resolve(property.returnType.asTypeName(
+//                nameResolver, classProto::getTypeParameter, false))
+//
+//            log("name = $name, type = $type, property = $property, thing = ${constructor.parameters[name]}")
+//        }
+
+        /*******/
+
+        val executableBits = ElementFilter.methodsIn(targetClass.enclosedElements)
         val setterPrefix = targetClass.setterPrefix
         val primaryConstructor = constructors.sortedBy { it.parameters.size }.last()
         val constructorParams = primaryConstructor.parameters
-        return constructorParams.map { it.asBuilderField(setterPrefix) }
+            return constructorParams.map { it.asBuilderField(setterPrefix) }
+            .plus(executableBits
+                .map { it.asBuilderField("blah", targetClass.kotlinMetadata as KotlinClassMetadata) }
+                .filterNotNull()
+            )
     }
 
     private fun log(any: Any) {
@@ -168,6 +204,27 @@ class JvmBuilderAnnotationProcessor : AbstractProcessor() {
         }
     }
 
+    private fun ExecutableElement.asBuilderField(setterPrefix: String, kotlinMetadata: KotlinClassMetadata): BuilderField? {
+//        log("Looking at $simpleName ---- ${kotlinMetadata.data.classProto.companionObjectName}")
+        if (simpleName.startsWith("get")) {
+//            log("\tWorking with $simpleName")
+            val property = kotlinMetadata.data.getPropertyOrNull(this)
+            val function = kotlinMetadata.data.classProto.propertyOrBuilderList
+//            log("\tProperty is $property")
+//            log("\tFunction is $function")
+            property?.let {
+                property.returnType?.nullable
+                return BuilderField(
+                    name = kotlinMetadata.data.nameResolver.getName(property.name).asString(),
+                    type = asType().asTypeName(), //.corrected(),
+                    setterPrefix = "${setterPrefix}_metadata"
+                )
+            }
+        }
+        return null
+    }
+
+
     data class BuilderField(val name: String, val type: TypeName, val setterPrefix: String) {
         fun asFunSpec(builderClass: String): FunSpec {
             return FunSpec.builder(generateSetterName())
@@ -190,12 +247,14 @@ class JvmBuilderAnnotationProcessor : AbstractProcessor() {
 
 private fun TypeName.asNullable(): TypeName = copy(nullable = true)
 
-private fun VariableElement.asBuilderField(setterPrefix: String): BuilderField =
-    BuilderField(
+private fun VariableElement.asBuilderField(setterPrefix: String): BuilderField {
+    return BuilderField(
         name = simpleName.toString(),
         type = asType().asTypeName().corrected(),
         setterPrefix = setterPrefix
     )
+}
+
 
 private val Element.setterPrefix: String
     get() = getAnnotation(JvmBuilder::class.java).setterPrefix
@@ -203,3 +262,93 @@ private val Element.setterPrefix: String
 private fun TypeName.corrected(): TypeName {
     return if (this.toString() == "java.lang.String") ClassName("kotlin", "String") else this
 }
+
+internal fun ProtoBuf.Type.asTypeName(
+    nameResolver: NameResolver,
+    getTypeParameter: (index: Int) -> ProtoBuf.TypeParameter,
+    useAbbreviatedType: Boolean = true
+): TypeName {
+
+    val argumentList = when {
+        useAbbreviatedType && hasAbbreviatedType() -> abbreviatedType.argumentList
+        else -> argumentList
+    }
+
+    if (hasFlexibleUpperBound()) {
+        return WildcardTypeName.producerOf(
+            flexibleUpperBound.asTypeName(nameResolver, getTypeParameter, useAbbreviatedType))
+            .copy(nullable = nullable)
+    } else if (hasOuterType()) {
+        return WildcardTypeName.consumerOf(
+            outerType.asTypeName(nameResolver, getTypeParameter, useAbbreviatedType))
+            .copy(nullable = nullable)
+    }
+
+    val realType = when {
+        hasTypeParameter() -> return getTypeParameter(typeParameter)
+            .asTypeName(nameResolver, getTypeParameter, useAbbreviatedType)
+            .copy(nullable = nullable)
+        hasTypeParameterName() -> typeParameterName
+        useAbbreviatedType && hasAbbreviatedType() -> abbreviatedType.typeAliasName
+        else -> className
+    }
+
+    var typeName: TypeName =
+        ClassName.bestGuess(nameResolver.getString(realType)
+            .replace("/", "."))
+
+    if (argumentList.isNotEmpty()) {
+        val remappedArgs: Array<TypeName> = argumentList.map { argumentType ->
+            val nullableProjection = if (argumentType.hasProjection()) {
+                argumentType.projection
+            } else null
+            if (argumentType.hasType()) {
+                argumentType.type.asTypeName(nameResolver, getTypeParameter, useAbbreviatedType)
+                    .let { argumentTypeName ->
+                        nullableProjection?.let { projection ->
+                            when (projection) {
+                                ProtoBuf.Type.Argument.Projection.IN -> WildcardTypeName.consumerOf(argumentTypeName)
+                                ProtoBuf.Type.Argument.Projection.OUT -> WildcardTypeName.producerOf(argumentTypeName)
+                                ProtoBuf.Type.Argument.Projection.STAR -> STAR
+                                ProtoBuf.Type.Argument.Projection.INV -> TODO("INV projection is unsupported")
+                            }
+                        } ?: argumentTypeName
+                    }
+            } else {
+                STAR
+            }
+        }.toTypedArray()
+        typeName = (typeName as ClassName).parameterizedBy(*remappedArgs)
+    }
+
+    return typeName.copy(nullable = nullable)
+}
+
+internal fun ProtoBuf.TypeParameter.asTypeName(
+    nameResolver: NameResolver,
+    getTypeParameter: (index: Int) -> ProtoBuf.TypeParameter,
+    resolveAliases: Boolean = false
+): TypeVariableName {
+    val possibleBounds = upperBoundList.map {
+        it.asTypeName(nameResolver, getTypeParameter, resolveAliases)
+    }
+    return if (possibleBounds.isEmpty()) {
+        TypeVariableName(
+            name = nameResolver.getString(name),
+            variance = variance.asKModifier())
+    } else {
+        TypeVariableName(
+            name = nameResolver.getString(name),
+            bounds = *possibleBounds.toTypedArray(),
+            variance = variance.asKModifier())
+    }
+}
+
+internal fun ProtoBuf.TypeParameter.Variance.asKModifier(): KModifier? {
+    return when (this) {
+        ProtoBuf.TypeParameter.Variance.IN -> KModifier.IN
+        ProtoBuf.TypeParameter.Variance.OUT -> KModifier.OUT
+        ProtoBuf.TypeParameter.Variance.INV -> null
+    }
+}
+
